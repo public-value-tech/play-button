b6f372f6e476acdccc7a864359271bb1f9ab2ed8
Alexander Türk <alx91@me.com>
2022-11-07 17:16:39 +0100

    Use swift 5.6 for spm. Pin snapshot testing version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.10.0")
+		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", exact: "1.9.0")
   ],
   targets: [
     .target(
